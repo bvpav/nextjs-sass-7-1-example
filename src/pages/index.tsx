a9ff64ca4e8ec68caaef648629a86f411b608bfa
bvpav <78971592+bvpav@users.noreply.github.com>
2023-03-06 10:28:40 +0200

    Made it work w/ sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,11 +13,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
+      <main className="main">
+        <div className="description">
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
+            <code className="code">src/pages/index.tsx</code>
           </p>
           <div>
             <a
@@ -26,11 +25,11 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
-                className={styles.vercelLogo}
+                className="vercelLogo"
                 width={100}
                 height={24}
                 priority
@@ -39,16 +38,16 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.center}>
+        <div className="center">
           <Image
-            className={styles.logo}
+            className="logo"
             src="/next.svg"
             alt="Next.js Logo"
             width={180}
             height={37}
             priority
           />
-          <div className={styles.thirteen}>
+          <div className="thirteen">
             <Image
               src="/thirteen.svg"
               alt="13"
@@ -59,10 +58,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.grid}>
+        <div className="grid">
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="card"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -76,7 +75,7 @@ export default function Home() {
 
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="card"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -90,7 +89,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="card"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -104,7 +103,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="card"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -119,5 +118,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
